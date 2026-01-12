a7c4c4f2c18f1258a2e08ce59e683fd748206546
Shreya19-777 <shreyaprasad777@gmail.com>
2026-01-12 12:04:41 -0500

    Test Code for imports
    added test code

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("hello sysc2010")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+print("Libraries imported successfully!")
+# Create a small series
+data = pd.Series([1, 3, 5, np.nan, 6, 8])
+print(data)
+
+#Loading the CSV file
+file = 'env_temp_humidity_clean.csv'
+df = pd.read_csv(file)
