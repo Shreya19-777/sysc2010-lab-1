317b6d4761e8b787c027653be9c62b64898be847
Shreya19-777 <shreyaprasad777@gmail.com>
2026-01-19 13:18:46 -0500

    added part 6 and fixed 5.3

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
+'''
 print("Libraries imported successfully!")
 # Create a small series
 data = pd.Series([1, 3, 5, np.nan, 6, 8])
@@ -14,36 +15,41 @@ file = 'env_temp_humidity_clean.csv'
 df = pd.read_csv(file, header=None)
 df.columns = ["Column1", "Column2", "Column3"]
 print(df.loc[0, "Column1"])
+'''
 
-
-#Part 5: Hands on Tasks
+#Part 5: ***********************Hands on Tasks*******************************
 #5.1 Plotting a Frequency Distribution
 def plot_freq ():
     f = 10
     fs = 500
     A = 5
-    duration =1
     t = np.arange(0, 1, 1/fs)
     y = A * np.sin(2 * np.pi * f * t)
+    
+    #Plotting the frequency
     plt.plot(t,y)
     plt.xlabel("Time (s)")
     plt.ylabel("Amplitude")
     plt.title("Sine Wave")
     plt.show()
     print("Frequency plot complete.")
+    
+
 #5.2 Generating Random Noise
 def sample_noises ():
     f = 10
     fs = 500
     A = 5
-    duration =1
+    
     t = np.arange(0, 1, 1/fs)
-    y = noisy_signal
     noise_sample = []
     noise_sample = np.random.normal(0, 1, 500) 
     signal = (A * np.sin(2 * np.pi * f * t))
     noisy_signal = []
     noisy_signal = 0.25 * noise_sample + signal
+    
+    t = np.arange(0, 1, 1/fs)
+    y = noisy_signal
 
     plt.plot(t, y)
     plt.xlabel("Time (s)")
@@ -79,17 +85,44 @@ def plot_temp () :
     x = np.array(times_to_plot)
     
     plt.plot(x,y)
-    plt.title("Tempraature Readings Over Time")
-    plt.ylabel("Temprature (C)")
+    plt.title("Temperature Readings Over Time")
+    plt.ylabel("Temperature (C)")
     plt.xlabel("Time (s)")
     plt.show()
 
+#Part 6: Exploring and Handling Sensor Data*************************************
+def load_csv() :
+    df = pd.read_csv('env_temp_humidity_clean.csv')
+    x = np.array(df.loc[0:1441, "temperature_C"].tolist())
+    y = np.array(df.loc[0:1441, "humidity_pct"].tolist())
+    
+    column_names = df.columns.tolist()
+    print(column_names)
+    plt.scatter(x,y)
+    plt.show()
+    
+    # Convert timestamp to actual datetime objects
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
 
+    # Display key info
+    print("--- First 5 Rows ---")
+    print(df.head())
+
+    print("\n--- Column Names ---")
+    print(df.columns)
+
+    print("\n--- Data Types ---")
+    print(df.dtypes)
+
+
+
+#Calling the functions
 if __name__ == "__main__" :
     #sample_temps()
-    plot_temp()
-    plot_freq()
-    sample_noises ()
+    #plot_temp()
+    #plot_freq()
+    #sample_noises()
+    load_csv()
 
 
 
